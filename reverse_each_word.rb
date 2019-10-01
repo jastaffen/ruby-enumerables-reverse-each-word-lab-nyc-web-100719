1a498c5c6ec0a545bffc067337adba565ab2d650
Jack Staffen <jack.staffen@gmail.com>
2019-10-01 16:37:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   #arr = string.split(" ")
-  new_arr.join(" ") = string.split(" ").collect { |word| word.reverse}
-  #new_arr.join(" ")
+  new_arr = string.split(" ").collect { |word| word.reverse}
+  new_arr.join(" ")
 end
